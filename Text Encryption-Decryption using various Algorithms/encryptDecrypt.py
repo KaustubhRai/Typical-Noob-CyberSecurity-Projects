c7d513ae4b47919591fcbd4aa039b9d107ad0992
KaustubhRai <raikaustubh12@gmail.com>
2023-09-29 20:10:17 +0530

    added code for vigenere cipher

--- Text Encryption-Decryption using various Algorithms/encryptDecrypt.py
+++ Text Encryption-Decryption using various Algorithms/encryptDecrypt.py
@@ -24,3 +24,35 @@ def caesar_decrypt(dec_text):
             decrypted_word += alphabets[new_index]
         decrypted_text += decrypted_word + " "
     return decrypted_text.rstrip()
+
+def vig_encrypt(enc_text):
+    key = input("\n enter the key with which you want this encrypted\n")
+    key = key.upper()
+    enc_text = enc_text.upper()
+    encrypted_text = ""
+    for i in range(len(enc_text)):
+        if enc_text[i] == " ":
+            encrypted_text += " "
+        else:
+            enc_text_index = ord(enc_text[i]) - ord("A")
+            key_index = ord(key[i % len(key)]) - ord("A")
+            new_index = (enc_text_index + key_index) % 26
+            new_char = chr(ord("A") + new_index)
+            encrypted_text += new_char
+    return encrypted_text
+
+def vig_decrypt(dec_text):
+    key = input("\n enter the key with which you want this decrypted\n")
+    key = key.upper()
+    dec_text = dec_text.upper()
+    decrypted_text = ""
+    for i in range(len(dec_text)):
+        if dec_text[i] == " ":
+            decrypted_text += " "
+        else:
+            dec_text_index = ord(dec_text[i]) - ord("A")
+            key_index = ord(key[i % len(key)]) - ord("A")
+            new_index = (dec_text_index - key_index) % 26
+            new_char = chr(ord("A") + new_index)
+            decrypted_text += new_char
+    return decrypted_text
--- main.py
+++ main.py
@@ -14,19 +14,62 @@ def main():
                 decision = int(input("\nWhat do you want to do?\n 1] Encrypt \n 2] Decrypt\n"))
                 match decision:
                     case 1:
-                        print("\n what algorithm do you want to encrypt in?\n a] caesar\n")
+                        print("\n what algorithm do you want to encrypt in?\n a] Caesar Cipher\n b] Substitution Cipher \n c] Vigenère Cipher \n d] Transposition Cipher \n e] DES \n f] AES \n g] RSA \n h] Diffie-Hellman Key Exchange \n i] Elliptic Curve Cryptography \n")
                         encryption_decision = input()
                         match encryption_decision:
                             case "a":
+                                print("\nChoose to go with Caesar Cipher\n")
                                 text = encryptDecrypt.caesar_encrypt(spam)
-                                print(text, "\n")
+                                print("Encrypted Text is -",text, "\n")
+                            case "b":
+                                print("\nChoose to go with Substitution Cipher\n")
+                                text = encryptDecrypt.caesar_encrypt(spam)
+                                print("Encrypted Text is -",text, "\n")
+                            case "c":
+                                print("\nChoose to go with Vigenère Cipher\n")
+                                text = encryptDecrypt.vig_encrypt(spam)
+                                print("Encrypted Text is -",text, "\n")
+                            case "d":
+                                print("\nChoose to go with Transposition Cipher\n")
+                            case "e":
+                                print("\nChoose to go with DES\n")
+                            case "f":
+                                print("\nChoose to go with AES\n")
+                            case "g":
+                                print("\nChoose to go with RSA\n")
+                            case "h":
+                                print("\nChoose to go with Diffie-Hellman Key Exchange\n")
+                            case "i":
+                                print("\nChoose to go with Elliptic Curve Cryptography\n")
+
                     case 2: 
-                        print("\n what algorithm do you want to decrypt in?\n a] caesar\n")
+                        print("\n what algorithm do you want to decrypt in?\n a] Caesar Cipher\n b] Substitution Cipher \n c] Vigenère Cipher \n d] Transposition Cipher \n e] DES \n f] AES \n g] RSA \n h] Diffie-Hellman Key Exchange \n i] Elliptic Curve Cryptography \n")
                         decryption_decision = input()
                         match decryption_decision:
                             case "a":
+                                print("\nChoose to go with Caesar Cipher\n")
                                 text = encryptDecrypt.caesar_decrypt(spam)
-                                print(text, "\n")
+                                print("Decrypted Text is -",text, "\n")
+                            case "b":
+                                print("\nChoose to go with Substitution Cipher\n")
+                                text = encryptDecrypt.caesar_decrypt(spam)
+                                print("Decrypted Text is -",text, "\n")
+                            case "c":
+                                print("\nChoose to go with Vigenère Cipher\n")
+                                text = encryptDecrypt.vig_decrypt(spam)
+                                print("Decrypted Text is -",text, "\n")
+                            case "d":
+                                print("\nChoose to go with Transposition Cipher\n")
+                            case "e":
+                                print("\nChoose to go with DES\n")
+                            case "f":
+                                print("\nChoose to go with AES\n")
+                            case "g":
+                                print("\nChoose to go with RSA\n")
+                            case "h":
+                                print("\nChoose to go with Diffie-Hellman Key Exchange\n")
+                            case "i":
+                                print("\nChoose to go with Elliptic Curve Cryptography\n")
         continue_or_exit = input("\n Do you want to continue (y/n)? ")
         if continue_or_exit != "y":
             break
